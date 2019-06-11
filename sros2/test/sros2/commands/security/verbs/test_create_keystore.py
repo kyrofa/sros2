4e459fbeab35b3b24beebed43f633651f3c13ae2
Emerson Knapp <eknapp@amazon.com>
2019-06-11 16:46:00 -0400

    Begin basic validation generated create_keystore generated files

--- sros2/test/sros2/commands/security/verbs/test_create_keystore.py
+++ sros2/test/sros2/commands/security/verbs/test_create_keystore.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import configparser
 import os
 import tempfile
 
@@ -19,11 +20,65 @@ from ros2cli import cli
 
 
 def test_create_keystore():
+    def check_governance_p7s(generated_file):
+        # TODO
+        pass
+
+    def check_index_txt(generated_file):
+        lines = generated_file.readlines()
+        assert len(lines) == 0
+
+    def check_ca_cert_pem(generated_file):
+        lines = generated_file.readlines()
+        assert lines[0] == '-----BEGIN CERTIFICATE-----\n'
+        assert lines[-1] == '-----END CERTIFICATE-----\n'
+
+    def check_ca_conf(generated_file):
+        config = configparser.ConfigParser()
+        config.read_file(generated_file)
+        assert config.sections() == [
+            ' ca ',
+            ' CA_default ',
+            ' policy_match ',
+            ' local_ca_extensions ',
+            ' req ',
+            ' req_distinguished_name ',
+            ' root_ca_extensions ',
+        ]
+
+    def check_ecdsaparam(generated_file):
+        lines = generated_file.readlines()
+        assert lines[0] == '-----BEGIN EC PARAMETERS-----'
+        assert lines[-1] == '-----END EC PARAMETERS-----'
+
+    def check_governance_xml(generated_file):
+        # TODO
+        pass
+
+    def check_ca_key_pem(generated_file):
+        # TODO
+        pass
+
+    def check_serial(generated_file):
+        # TODO
+        pass
+
     with tempfile.TemporaryDirectory() as keystore_dir:
         assert cli.main(argv=['security', 'create_keystore', keystore_dir]) == 0
-        expected_files = [
-            'governance.p7s', 'index.txt', 'ca.cert.pem', 'ca_conf.cnf', 'ecdsaparam',
-            'governance.xml', 'ca.key.pem', 'serial'
-        ]
-        for expected_file in expected_files:
-            assert os.path.isfile(os.path.join(keystore_dir, expected_file))
+        expected_files = (
+            ('governance.p7s', check_governance_p7s),
+            ('index.txt', check_index_txt),
+            ('ca.cert.pem', check_ca_cert_pem),
+            ('ca_conf.cnf', check_ca_conf),
+            ('ecdsaparam', check_ecdsaparam),
+            ('governance.xml', check_governance_p7s),
+            ('ca.key.pem', check_ca_key_pem),
+            ('serial', check_serial),
+        )
+
+        for expected_file, file_validator in expected_files:
+            path = os.path.join(keystore_dir, expected_file)
+            assert os.path.isfile(path), 'Expected output file %s was not found.' % expected_file
+            if file_validator:
+                with open(path, 'r') as generated_file:
+                    file_validator(generated_file)
